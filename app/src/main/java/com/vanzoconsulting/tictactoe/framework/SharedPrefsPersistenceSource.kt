e3870fe62a462845986979e8db43e39c2e15e5e6
Babak ZO <babak@vanzoconsulting.com>
2020-05-17 22:15:44 +0200

    #143 Phase 1: game start and board initialization. Providing the shared preferences implementation of the persistence source

--- app/src/main/java/com/vanzoconsulting/tictactoe/framework/SharedPrefsPersistenceSource.kt
+++ app/src/main/java/com/vanzoconsulting/tictactoe/framework/SharedPrefsPersistenceSource.kt
@@ -1,10 +1,27 @@
 package com.vanzoconsulting.tictactoe.framework
 
+import android.content.SharedPreferences
 import com.vanzoconsulting.domain.Board
+import com.vanzoconsulting.domain.Player.Companion.valueOf
 import com.vanzoconsulting.persistence.BoardPersistenceSource
 
-class SharedPrefsPersistenceSource : BoardPersistenceSource {
+class SharedPrefsPersistenceSource(private val prefs: SharedPreferences): BoardPersistenceSource {
 
-    override fun loadBoard(): Board? = null
-    override fun saveBoard(board: Board) = false
+    override fun loadBoard(): Board? =
+        prefs.getString(PREF_KEY_BOARD, null)
+            ?.replace("[", "")  //remove the right bracket
+            ?.replace("]", "")  //remove the left bracket
+            ?.split(", ")              //separate by comma
+            ?.map { it -> valueOf(it) }
+            ?.toTypedArray()
+            ?.let {
+                Board(it)
+            }
+
+    override fun saveBoard(board: Board) =
+        prefs.edit().putString(PREF_KEY_BOARD, board.toString()).commit()
+
+    companion object {
+        const val PREF_KEY_BOARD = "board"
+    }
 }
--- app/src/test/java/com/vanzoconsulting/tictactoe/framework/SharedPrefsPersistenceSourceTest.kt
+++ app/src/test/java/com/vanzoconsulting/tictactoe/framework/SharedPrefsPersistenceSourceTest.kt
@@ -1,14 +1,56 @@
 package com.vanzoconsulting.tictactoe.framework
 
+import android.content.SharedPreferences
 import com.vanzoconsulting.domain.Board
 import com.vanzoconsulting.domain.Player.O
 import com.vanzoconsulting.domain.Player.X
+import com.vanzoconsulting.tictactoe.framework.SharedPrefsPersistenceSource.Companion.PREF_KEY_BOARD
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations.initMocks
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 internal class SharedPrefsPersistenceSourceTest {
 
-    private val persistenceSource = SharedPrefsPersistenceSource()
+    @Mock
+    private lateinit var prefs: SharedPreferences
+
+    @InjectMocks
+    private lateinit var persistenceSource: SharedPrefsPersistenceSource
+
+    @Before
+    fun setUp() {
+        initMocks(this)
+    }
+
+    @Test
+    fun loadBoard() {
+        `when`(
+            prefs.getString(
+                PREF_KEY_BOARD,
+                null
+            )
+        ).thenReturn("[X, O, X, O, X, null, null, null, null]")
+
+        val expectedBoard = Board(
+            arrayOf(
+                X, O, X,
+                O, X, null,
+                null, null, null
+            )
+        )
+        val actualBoard = persistenceSource.loadBoard()
+
+        assertEquals(expectedBoard, actualBoard)
+    }
 
     @Test
     fun saveBoard() {
@@ -20,22 +62,16 @@ internal class SharedPrefsPersistenceSourceTest {
             )
         )
 
+        val editor = mock(SharedPreferences.Editor::class.java)
+        `when`(editor.putString(anyString(), anyString())).thenReturn(editor)
+        `when`(editor.commit()).thenReturn(true)
+
+        `when`(prefs.edit()).thenReturn(editor)
+
         val saved = persistenceSource.saveBoard(board)
 
-        assertEquals(true, saved)
-    }
-
-    @Test
-    fun loadBoard() {
-        val expectedBoard = Board(
-            arrayOf(
-                X, O, X,
-                O, X, null,
-                null, null, null
-            )
-        )
-        val actualBoard = persistenceSource.loadBoard()
-
-        assertEquals(expectedBoard, actualBoard)
+        assertTrue(saved)
+        verify(editor).putString(PREF_KEY_BOARD, board.toString())
+        verify(editor).commit()
     }
 }
